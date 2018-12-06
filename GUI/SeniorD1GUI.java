24580897a31cb4949e6fca64f28d238e96579864
Matthew Whitlock <MatthewWhitlock.MW@gmail.com>
2018-12-05 19:01:23 -0600

    Update GUI

--- GUI/SeniorD1GUI.java
+++ GUI/SeniorD1GUI.java
@@ -1,140 +1,316 @@
 import javax.swing.*;
+import javax.imageio.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import java.util.*;
 import java.io.*;
-import javax.swing.*;
+import java.text.SimpleDateFormat;
 import javax.swing.event.*;
 import java.awt.Graphics.*;
 import java.lang.*;
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+//import java.awt.Dialog.ModalityType;
 
 public class SeniorD1GUI{
-	private static int x = 0;
-	private static int y = 0;
-	private static boolean pressed = false;
-	private static boolean check = false;
-	private static int tempX, tempY;
-	private static GUIFrame object = new GUIFrame();
-	private static long count;
-	private static int jpg = 0;
-	/*
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        ImageIcon i = new ImageIcon("GUI.png");
-		if (jpg == 0) i = new ImageIcon("GUI.jpg");
-		else if (jpg == 1) i = new ImageIcon("GUI_DG_pressed.jpg");
-		else if (jpg == 2) i = new ImageIcon("GUI_M_pressed.jpg");
-		else if (jpg == 3) i = new ImageIcon("GUI_S_pressed.jpg");
-        i.paintIcon(this, g, 0, 0);
-    }
-	*/
+	private static JFrame frame;
+	private static JLabel distance;
+	private static JLabel bottomText2;
+	private static JSplitPane plotPanel;
+	private static JPanel filesPanel;
+	private static JPanel graphPanel;
+	private static double speedOfSound = 5400.0;
+	private static double calibrationSpeed = -1;
+	private static double nCal = -1;
 	
-    public static void main(String[] args){
-        //SeniorD1GUI inter = new SeniorD1GUI();
-        JFrame jf = new JFrame();
-        jf.setTitle("Senior Design GUI");
-        jf.setSize(1295,760);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.add(object);
-		object.addMouseListener(new AL());
-    }
+	private static final String mainPanelID = "mainPanel";
+	private static final String plotPanelID = "plotPanel";
+	private static final String fileSuffix = ".ndt";
 	
-	static class AL extends MouseAdapter{
-		public void mousePressed (MouseEvent e){
-			x = e.getX();
-			y = e.getY();
-			tempX = x;
-			tempY = y;
-			if (!check){
-				if (y >= 196 && y < 614){
-					if (x >= 0 && x < 430) jpg = 1;
-					else if (x >= 430 && x < 852) jpg = 2;
-					else if (x >= 852 && x < 1290) jpg = 3;
-				}
-			}
-			else {
-				if (y >= 16 && y <= 73 && x >= 18 && x <= 113) {
-					pressed = true;
-				}
-			}
-			object.framing(x, y, jpg, pressed);
-			x = 0;
-			y = 0;
+	private static boolean alreadyOpenedPlot = false;
+	
+	public static void main(String args[]){
+		frame = new JFrame("Senior Design One - Ultrasonic Flaw Detection");
+		
+		CardLayout cardLayout = new CardLayout();
+		JPanel cards = new JPanel(cardLayout);
+		JPanel mainPanel = new JPanel(new GridBagLayout());
+		cards.add(mainPanel, mainPanelID);
+		cardLayout.show(cards, mainPanelID);
+		
+		Font generalFont = new Font("Copperplate Gothic", Font.PLAIN, 25);
+		Font boldFont = new Font("Copperplate Gothic", Font.BOLD, 25);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 3;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		
+		
+		Color osuOrange = new Color(255, 106, 20);
+		BufferedImage myPicture = null;
+		try{
+			myPicture = ImageIO.read(new File("starwars_title.png"));
+		} catch (Exception e){
+			//Do nothing b/c I don't care.
 		}
-		public void mouseReleased (MouseEvent e){
-			x = e.getX();
-			y = e.getY();
-			if (!check){
-				if (y >= 196 && y < 614){
-					if (x >= 0 && x < 430 && jpg == 1) check = true;
-					else if (x >= 430 && x < 852 && jpg == 2) jpg = 0;
-					else if (x >= 852 && x < 1290 && jpg == 3) check = true;
-					else jpg = 0;
+		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBackground(osuOrange);
+		titlePanel.add(picLabel);
+		mainPanel.add(titlePanel, gbc);
+		
+		JButton data = new JButton("Data & Graph");
+		data.setFont(generalFont);
+		gbc.weighty = 1;
+		gbc.gridwidth = 1;
+		gbc.gridy = 1;
+		mainPanel.add(data, gbc);
+		
+		JButton measure = new JButton("Measure");
+		measure.addActionListener(e -> doMeasureAndShow());
+		measure.setFont(generalFont);
+		gbc.gridx = 1;
+		mainPanel.add(measure, gbc);
+		
+		JButton calibrate = new JButton("Calibrate");
+		calibrate.addActionListener(e -> doCalibrate());
+		calibrate.setFont(generalFont);
+		gbc.gridx = 2;
+		mainPanel.add(calibrate, gbc);
+		
+		JPanel distancePanel = new JPanel(new GridBagLayout());
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.weighty = 0;
+		gbc.gridwidth = 3;
+		mainPanel.add(distancePanel, gbc);
+		
+		JLabel bottomText = new JLabel("Distance to flaw = ");
+		bottomText.setFont(generalFont);
+		distance = new JLabel("12");
+		distance.setFont(boldFont);
+		bottomText2 = new JLabel(" cm (5400 m/s)");
+		bottomText2.setFont(generalFont);
+		JLabel filler1 = new JLabel();
+		JLabel filler2 = new JLabel();
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.gridwidth = 1;
+		distancePanel.add(filler1, gbc);
+		gbc.gridx = 1;
+		gbc.weightx = 0;
+		distancePanel.add(bottomText, gbc);
+		gbc.gridx = 2;
+		distancePanel.add(distance, gbc);
+		gbc.gridx = 3;
+		distancePanel.add(bottomText2, gbc);
+		gbc.gridx = 4;
+		gbc.weightx = 1;
+		distancePanel.add(filler2,gbc);
+		
+		//Make plotting panel stuff
+		filesPanel = new JPanel(new BorderLayout());
+		graphPanel = new JPanel(new GridBagLayout());
+		plotPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, filesPanel, graphPanel);
+		
+		gbc.weighty = 0;
+		gbc.gridy = 2;
+		JButton returnButton = new JButton("Return to main menu");
+		returnButton.addActionListener(e -> cardLayout.show(cards, mainPanelID));
+		filesPanel.add(returnButton, BorderLayout.SOUTH);
+		
+		data.addActionListener(e -> {
+			if(alreadyOpenedPlot) filesPanel.remove(((BorderLayout)filesPanel.getLayout()).getLayoutComponent(BorderLayout.CENTER));
+			alreadyOpenedPlot = true;
+			File dir = new File(".");
+			File[] filesList = dir.listFiles();
+			ArrayList<String> filesToDisp = new ArrayList<String>();
+			for (File file : filesList) {
+				if (file.isFile() && file.getName().toLowerCase().endsWith(fileSuffix)) {
+					filesToDisp.add(file.getName());
 				}
-				else jpg = 0;
 			}
-			else{
-				if (y >= 16 && y <= 73 && x >= 18 && x <= 113) {
-					check = false;
-					jpg = 0;
+			
+			JList<String> fileList = new JList<String>(filesToDisp.toArray(new String[filesToDisp.size()]));
+			fileList.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
+			
+			MouseListener mouseListener = new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) {
+				   String selectedItem = fileList.getSelectedValue();
+				   doPlot(selectedItem);
+				}
+			};
+			fileList.addMouseListener(mouseListener);
+			
+			fileList.setMinimumSize(returnButton.getMinimumSize());
+			fileList.setPreferredSize(returnButton.getMinimumSize());
+			
+			filesPanel.add(fileList, BorderLayout.CENTER);
+			
+			cardLayout.show(cards, plotPanelID);
+		});
+		
+		cards.add(plotPanel, plotPanelID);
+				
+		frame.add(cards);
+		frame.setSize(1000,600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	private static void doPlot(String filename){
+		System.out.println(filename);
+	}
+	
+	private static void updateSpeed(double newSpeed){
+		speedOfSound = newSpeed;
+		bottomText2.setText(" cm (" + Double.toString(speedOfSound) + " m/s)");
+	}
+	
+	private static double doMeasure(){
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+		String fileName = timeStamp + fileSuffix;
+		return doMeasure(fileName);
+	}
+	
+	private static double doMeasure(String s){
+		String fileName = s;
+		Runtime rt = Runtime.getRuntime();
+		double distanceToFlaw = 0;
+		try{
+			Process pr = rt.exec("sudo chrt -f 90 taskset -c 2 ./register_level_gpio.c " + fileName);
+			pr.waitFor();
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			String line = reader.readLine();
+			while(line != null){
+				if(Float.parseFloat(line.split(",")[0]) > 0.3){
+					distanceToFlaw = Double.parseDouble(line.split(",")[1]) / 2 * 0.000001 * speedOfSound;
+					break;
 				}
 			}
-			pressed = false;
-			object.released(x,y,jpg, pressed, check);
-			x = 0;
-			y = 0;
-		}	
+		} catch(Exception e){
+			//Who cares?
+		}
+		return distanceToFlaw;
+	}
+	
+	private static void doMeasureAndShow(){
+		distance.setText(Double.toString(doMeasure()));
+	}
+	
+	private static void setCalSpeed(double distanceToFlaw){
+		String fileName = "temp";
+		Runtime rt = Runtime.getRuntime();
+		//double distanceToFlaw = 0;
+		try{
+			Process pr = rt.exec("sudo chrt -f 90 taskset -c 2 ./register_level_gpio.c " + fileName);
+			pr.waitFor();
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			String line = reader.readLine();
+			while(line != null){
+				if(Float.parseFloat(line.split(",")[0]) > 0.3){
+					double tempSpeed = distanceToFlaw*2/(Double.parseDouble(line.split(",")[1])*0.000001);
+					if (nCal == -1) {
+						calibrationSpeed = tempSpeed;
+						nCal = 1;
+					} else if (nCal > 0){
+						nCal++;
+						calibrationSpeed = (nCal-1)*calibrationSpeed/nCal + tempSpeed/nCal;
+					} 
+					return;
+				}
+			}
+		} catch(Exception e){
+			//Who cares?
+		}
+	}
+	
+	private static void doCalibrate(){
+		final String calpan = "calpan";
+		final String confirmpan = "confirmpan";
+		JDialog calibrationDialog = new JDialog(frame, "Calibration - Please enter distance to flaw");
+		JPanel topLevelCalPanel = new JPanel(new CardLayout());
+		JPanel calibrationPanel = new JPanel(new GridBagLayout());
+		topLevelCalPanel.add(calibrationPanel, calpan);
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 1;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		JTextField distanceField = new JTextField();
+		calibrationPanel.add(distanceField, gbc);
+		JButton readyButton = new JButton("   Ready   ");
+		gbc.gridx = 1;
+		gbc.weightx = 0.1;
+		calibrationPanel.add(readyButton, gbc);
+		
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.gridwidth = 2;
+		JPanel confirmPanel = new JPanel(new GridBagLayout());
+		JLabel confirmLabel = new JLabel();
+		confirmPanel.add(confirmLabel, gbc);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridy = 1;
+		gbc.gridwidth = 1;
+		JButton weGood = new JButton("Accept Value");
+		confirmPanel.add(weGood, gbc);
+		gbc.gridx = 1;
+		JButton nyb = new JButton("Continue Calibration");
+		confirmPanel.add(nyb, gbc);
+		topLevelCalPanel.add(confirmPanel, confirmpan);
+		((CardLayout) (topLevelCalPanel.getLayout())).show(topLevelCalPanel, calpan);
+		//calibrationDialog.setModalityExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDED);
+		calibrationDialog.setModal(true);
+		calibrationDialog.add(topLevelCalPanel);
+		calibrationDialog.pack();
+		calibrationDialog.setSize(300,70);
+		calibrationDialog.setLocationRelativeTo(null);
+		
+		
+		readyButton.addActionListener(e -> {
+			double distanceVal = 0;
+			try{
+				distanceVal = Double.parseDouble(distanceField.getText());
+			} catch (Exception ex){
+				System.out.println("Unable to parse: \"" + distanceField.getText() + "\"" );
+				return;
+			}
+			setCalSpeed(distanceVal);
+			confirmLabel.setText("Calibrated speed: " + Double.toString(calibrationSpeed) + " m/s");
+			((CardLayout) (topLevelCalPanel.getLayout())).show(topLevelCalPanel, confirmpan);
+			calibrationDialog.setSize(300,85);
+		});
+		
+		nyb.addActionListener(e -> {
+			((CardLayout) (topLevelCalPanel.getLayout())).show(topLevelCalPanel, calpan);
+			calibrationDialog.setSize(300,70);
+		});
+		
+		weGood.addActionListener(e -> {
+			updateSpeed(calibrationSpeed);
+			nCal = -1;
+			calibrationDialog.dispose();
+		});
+		
+		calibrationDialog.setVisible(true);
 	}
 }
 
-class GUIFrame extends JPanel {
-	public static int x, y, jpg;
-	public boolean check = false;
-	public boolean pressed = false;
-	
-	public void framing(int xx, int yy, int jj, boolean pp){
-		x = xx;
-		y = yy;
-		jpg = jj;
-		pressed = pp;
-		repaint();
-	}
-	
-	public void released(int xx, int yy, int jj, boolean pp, boolean cc){
-		x = xx;
-		y = yy;
-		jpg = jj;
-		pressed = pp;
-		check = cc;
-		repaint();
-	}
-	
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		ImageIcon i = new ImageIcon("GUI.jpg");
-		if (!check){
-			if (jpg == 0) i = new ImageIcon("GUI.jpg");
-			else if (jpg == 1) i = new ImageIcon("GUI_DG_pressed.jpg");
-			else if (jpg == 2) i = new ImageIcon("GUI_M_pressed.jpg");
-			else if (jpg == 3) i = new ImageIcon("GUI_S_pressed.jpg");
-		}
-		else {
-			if (jpg == 3){
-				if (!pressed) i = new ImageIcon("GUI_Specs.jpg");
-				else if (pressed) i = new ImageIcon("GUI_Specs_pressed.jpg");
-			}
-			if (jpg == 1){
-				if (!pressed) i = new ImageIcon("GUI_DataNGraphs.jpg");
-				else if (pressed) i = new ImageIcon("GUI_DataNGraphs_pressed.jpg");
-			}
-				
-		}
-        i.paintIcon(this, g, 0, 0);
-		g.setColor(Color.red);
-		g.drawString("x = " + x + ", y = " +y, 80, 80);
-	}
-}
 
 		
 		
